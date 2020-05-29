31b5afe7557c5f029db6f1cf3f9f5de4f8d58d03
etiennefd <etiennefd@gmail.com>
2020-05-28 22:37:59 -0400

    Ajoute des utilitaires pour la conversion de température et des enums

---
+++ Metaeo/Utilitaires.swift
@@ -0,0 +1,28 @@
+//
+//  Utilitaires.swift
+//  Metaeo
+//
+//  Created by Étienne Fortier-Dubois on 20-04-08.
+//  Copyright © 2020 Étienne Fortier-Dubois. All rights reserved.
+//
+
+import Foundation
+
+enum Condition: String {
+  case Clear, PartlyCloudy, Sunny
+}
+enum PointCardinal: String {
+  case N, NNE, NE, ENE, E, ESE, SE, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW
+}
+
+enum TendancePression: String {
+  case falling, rising, steady
+}
+
+func celsiusVersFahrenheit(_ celsius: Double) -> Double {
+  return celsius * 1.8 + 32
+}
+
+func fahrenheitVersCelsius(_ fahrenheit: Double) -> Double {
+  return (fahrenheit - 32) / 1.8
+}
