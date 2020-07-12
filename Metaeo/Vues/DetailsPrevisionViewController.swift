c27263a2a41bafb754ea933079f50cc01f4ba017
etiennefd <etiennefd@gmail.com>
2020-07-12 18:00:13 -0400

    Ajoute des fichiers pour les view controllers des paramètres et des détails de prévision

---
+++ Metaeo/Vues/DetailsPrevisionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsPrevisionViewController.swift
+//  Metaeo
+//
+//  Created by Étienne Fortier-Dubois on 20-07-12.
+//  Copyright © 2020 Étienne Fortier-Dubois. All rights reserved.
+//
+
+import UIKit
+
+class DetailsPrevisionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
