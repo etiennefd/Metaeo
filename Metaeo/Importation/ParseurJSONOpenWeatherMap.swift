32fe99a748f36ba4fefa49345e7701d7127e7f15
etiennefd <etiennefd@gmail.com>
2020-07-27 12:14:05 -0400

    Importe les conditions actuelles de OWM

--- Metaeo/Importation/ParseurJSONOpenWeatherMap.swift
+++ Metaeo/Importation/ParseurJSONOpenWeatherMap.swift
@@ -178,7 +178,7 @@ class ParseurJSONOpenWeatherMap: ParseurJSON {
       previsionsParJour[previsionNuit.heureDebut] = previsionNuit
     }
     
-    completionHandler(nil, previsionsParJour, previsionsParHeure)
+    completionHandler(conditionsActuelles, previsionsParJour, previsionsParHeure)
   }
 }
 
